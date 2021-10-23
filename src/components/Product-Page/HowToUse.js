3378ec669e66cb5abbb017ad213c8d42094f2be2
Jasmeet Singh <jasmeetsingh4real1984@gmail.com>
2021-10-23 18:03:01 +0530

    added HowTOUse.css

---
+++ src/components/Product-Page/HowToUse.js
@@ -0,0 +1,9 @@
+import "./HowToUse.css";
+const HowToUse = () => {
+  return (
+    <div>
+      <h1>How To Use</h1>
+    </div>
+  );
+};
+export default HowToUse;
--- src/components/Product-Page/ProductPage.js
+++ src/components/Product-Page/ProductPage.js
@@ -2,10 +2,12 @@ import React from "react";
 import BenifitAndUses from "./BenifitAndUses";
 import Footer from "./Footer";
 import TripleGb from "./TripleGb";
+import HowToUse from "./HowToUse";
 export default function Product() {
   return (
     <div>
       <TripleGb />
+      <HowToUse />
       <BenifitAndUses />
       <Footer />
     </div>
